449174a5a999aa5a969089d997f8f3c5a99d08b1
Horiuchi Fubuki <fubuki199914@gmail.com>
2018-05-21 17:13:18 +0900

    ルーレット回したよ

---
+++ Roulette/Assets/chapter3/RouletteController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+    float rotSpeed = 0;
+    int i = 0;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            i = 1;   
+        }
+        if (i == 1)
+        {
+            this.rotSpeed += 1;
+            if (this.rotSpeed > 50)
+                i = 0;
+        }
+        else
+        {
+            this.rotSpeed *= 0.96f;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);
+        
+    }
+}
